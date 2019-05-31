48364b541f4c18e79c0c4e4c71bca253988ed25b
Evgeniy Timokhov <timocov@gmail.com>
2019-05-31 10:36:23 +0300

    Changed max blank lines in JS code to 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -224,7 +224,7 @@ module.exports = {
 		'no-mixed-spaces-and-tabs': 'error',
 
 		// disallow multiple empty lines and only one newline at the end
-		'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+		'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
 
 		// disallow nested ternary expressions
 		// 'no-nested-ternary': 2,
