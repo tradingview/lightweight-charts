c8b7a5cda2ac5557713c6a18cf818f6af3e0c7db
Evgeniy Timokhov <timocov@gmail.com>
2019-06-14 13:30:02 +0300

    Fixed applying incorrect bar spacing
    - Added validation for bar spacing
    - Renamed _tryToUpdateBarSpacing to _setBarSpacing and put validation there
    - Tests

--- tests/e2e/graphics/test-cases/initial-options/invalid-bar-spacing.js
+++ tests/e2e/graphics/test-cases/initial-options/invalid-bar-spacing.js
@@ -1,7 +1,7 @@
 function generateData() {
 	var res = [];
 	var time = new Date(Date.UTC(2018, 0, 1, 0, 0, 0, 0));
-	for (var i = 0; i < 1000; ++i) {
+	for (var i = 0; i < 500; ++i) {
 		res.push({
 			time: time.getTime() / 1000,
 			value: i,
@@ -14,14 +14,13 @@ function generateData() {
 
 // eslint-disable-next-line no-unused-vars
 function runTestCase(container) {
-	var chart = LightweightCharts.createChart(container);
+	var chart = LightweightCharts.createChart(container, {
+		timeScale: {
+			barSpacing: 100,
+		},
+	});
 
-	var mainSeries = chart.addLineSeries();
+	var mainSeries = chart.addCandlestickSeries();
 
 	mainSeries.setData(generateData());
-
-	chart.timeScale().setVisibleRange({
-		from: (new Date(Date.UTC(2018, 0, 1, 0, 0, 0, 0))).getTime() / 1000,
-		to: (new Date(Date.UTC(2018, 1, 1, 0, 0, 0, 0))).getTime() / 1000,
-	});
 }
