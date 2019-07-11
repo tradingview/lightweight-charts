5591f8a7f829ca382c115acfdb6a3e236807688a
Kirill Chetverikov <kchetverikov@tradingview.com>
2019-07-11 20:21:15 +0300

    Fixed code defects

--- src/model/time-data.ts
+++ src/model/time-data.ts
@@ -49,8 +49,11 @@ function upperBoundItemsCompare(time: TimePointIndex, item: TimedValue): boolean
 }
 
 export function visibleTimedValues(items: TimedValue[], range: BarsRange, extendedRange: boolean): SeriesItemsIndexesRange {
-	const from = lowerbound<TimedValue, TimePointIndex>(items, range.firstBar(), lowerBoundItemsCompare);
-	const to = upperbound<TimedValue, TimePointIndex>(items, range.lastBar(), upperBoundItemsCompare);
+	const firstBar = range.firstBar();
+	const lastBar = range.lastBar();
+
+	const from = lowerbound<TimedValue, TimePointIndex>(items, firstBar, lowerBoundItemsCompare);
+	const to = upperbound<TimedValue, TimePointIndex>(items, lastBar, upperBoundItemsCompare);
 
 	if (!extendedRange) {
 		return { from, to };
@@ -59,11 +62,11 @@ export function visibleTimedValues(items: TimedValue[], range: BarsRange, extend
 	let extendedFrom = from;
 	let extendedTo = to;
 
-	if (from > 0 && items[from].time >= range.firstBar()) {
+	if (from > 0 && items[from].time >= firstBar) {
 		extendedFrom = from - 1;
 	}
 
-	if (to < items.length && to > 0 && items[to - 1].time <= range.lastBar()) {
+	if (to < items.length && to > 0 && items[to - 1].time <= lastBar) {
 		extendedTo = to + 1;
 	}
 
