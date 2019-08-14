1c11658cc6116d7c71454ecf315dbec7ea17fbc4
Kirill Chetverikov <kchetverikov@tradingview.com>
2019-08-14 20:30:17 +0300

    Updated series markers re-aligning test

--- tests/e2e/graphics/test-cases/series-markers/series-markers-re-aligned.js
+++ tests/e2e/graphics/test-cases/series-markers/series-markers-re-aligned.js
@@ -24,11 +24,23 @@ function runTestCase(container) {
 		{ time: '2017-04-24', position: 'inBar', color: 'orange', shape: 'circle' },
 	]);
 
-	line.update({ time: '2017-04-10', value: 85.01 });
-	line.update({ time: '2017-04-16', value: 85.01 });
-	line.update({ time: '2017-04-17', value: 80.01 });
-	line.update({ time: '2017-04-18', value: 85.01 });
-	line.update({ time: '2017-04-24', value: 85.01 });
+	line.setData([
+		{ time: '2017-04-10', value: 85.01 },
+		{ time: '2017-04-11', value: 80.01 },
+		{ time: '2017-04-12', value: 96.63 },
+		{ time: '2017-04-13', value: 76.64 },
+		{ time: '2017-04-14', value: 81.89 },
+		{ time: '2017-04-15', value: 74.43 },
+		{ time: '2017-04-16', value: 85.01 },
+		{ time: '2017-04-17', value: 80.01 },
+		{ time: '2017-04-18', value: 85.01 },
+		{ time: '2017-04-19', value: 91.89 },
+		{ time: '2017-04-20', value: 81.89 },
+		{ time: '2017-04-21', value: 91.89 },
+		{ time: '2017-04-22', value: 81.89 },
+		{ time: '2017-04-23', value: 91.89 },
+		{ time: '2017-04-24', value: 85.01 },
+	]);
 
 	chart.timeScale().fitContent();
 }
