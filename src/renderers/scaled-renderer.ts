7255f9e5ce3b63842f8545ddc33567ee931ce6cd
ekorobko <ekorobko@tradingview.com>
2019-12-23 18:37:20 +0300

    274: Fixed review issues

--- src/renderers/scaled-renderer.ts
+++ src/renderers/scaled-renderer.ts
@@ -3,9 +3,7 @@ import { IPaneRenderer } from './ipane-renderer';
 export abstract class ScaledRenderer implements IPaneRenderer {
 	public draw(ctx: CanvasRenderingContext2D, pixelRatio: number, isHovered: boolean, hitTestData?: unknown): void {
 		ctx.save();
-		if (Math.abs(pixelRatio - 1) > 0.01) {
-			ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-		}
+		ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
 		this._drawImpl(ctx, isHovered, hitTestData);
 		ctx.restore();
 	}
