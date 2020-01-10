fab585a1f46dbe5feca97fceff7ae6172a6f4816
Evgeniy Timokhov <timocov@gmail.com>
2020-01-10 13:48:50 +0300

    Replaced setTransform with scale and added note about scaling in ScaledRenderer

--- src/renderers/scaled-renderer.ts
+++ src/renderers/scaled-renderer.ts
@@ -3,6 +3,9 @@ import { IPaneRenderer } from './ipane-renderer';
 export abstract class ScaledRenderer implements IPaneRenderer {
 	public draw(ctx: CanvasRenderingContext2D, pixelRatio: number, isHovered: boolean, hitTestData?: unknown): void {
 		ctx.save();
+		// actually we must be sure that this scaling applied only once at the same time
+		// currently ScaledRenderer could be only nodes renderer (not top-level renderers like CompositeRenderer or something)
+		// so this "constraint" is fulfilled for now
 		ctx.scale(pixelRatio, pixelRatio);
 		this._drawImpl(ctx, isHovered, hitTestData);
 		ctx.restore();
@@ -10,7 +13,10 @@ export abstract class ScaledRenderer implements IPaneRenderer {
 
 	public drawBackground(ctx: CanvasRenderingContext2D, pixelRatio: number, isHovered: boolean, hitTestData?: unknown): void {
 		ctx.save();
-		ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+		// actually we must be sure that this scaling applied only once at the same time
+		// currently ScaledRenderer could be only nodes renderer (not top-level renderers like CompositeRenderer or something)
+		// so this "constraint" is fulfilled for now
+		ctx.scale(pixelRatio, pixelRatio);
 		this._drawBackgroundImpl(ctx, isHovered, hitTestData);
 		ctx.restore();
 	}
