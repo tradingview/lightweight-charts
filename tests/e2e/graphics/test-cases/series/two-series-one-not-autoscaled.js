5a232fdd7f23ab7254fd2d573028267a54c74301
Eugene Korobko <ekorobko@gmail.com>
2020-05-06 12:05:33 +0300

    392: Fixed and added tests

--- tests/e2e/graphics/test-cases/series/two-series-one-not-autoscaled.js
+++ tests/e2e/graphics/test-cases/series/two-series-one-not-autoscaled.js
@@ -1,10 +1,11 @@
-function generateData() {
+function generateData(amplitude) {
 	var res = [];
+	amplitude = amplitude || 30;
 	var time = new Date(Date.UTC(2018, 0, 1, 0, 0, 0, 0));
 	for (var i = 0; i < 500; ++i) {
 		res.push({
 			time: time.getTime() / 1000,
-			value: 50 + 30 * Math.sin(Math.PI * i / 50),
+			value: 50 + amplitude * Math.sin(Math.PI * i / 50),
 		});
 
 		time.setUTCDate(time.getUTCDate() + 1);
@@ -25,16 +26,18 @@ function runTestCase(container) {
 
 	var mainSeries = chart.addLineSeries({
 		lineWidth: 1,
+		color: '#0000ff',
+	});
+
+	mainSeries.setData(generateData(20));
+
+	var secondSeries = chart.addLineSeries({
+		lineWidth: 1,
 		color: '#ff0000',
 		autoscaleInfoProvider: function() {
-			return {
-				priceRange: {
-					minValue: 0,
-					maxValue: 100,
-				},
-			};
+			return null;
 		},
 	});
 
-	mainSeries.setData(generateData());
+	secondSeries.setData(generateData());
 }
