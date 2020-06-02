b649da80371b7fe6597abb3803b2d02d08c11cfd
Evgeniy Timokhov <timocov@gmail.com>
2020-06-02 17:26:24 +0300

    Removed unnecessary type cast

--- src/api/options/price-line-options-defaults.ts
+++ src/api/options/price-line-options-defaults.ts
@@ -1,10 +1,9 @@
-import { BarPrice } from '../../model/bar';
 import { PriceLineOptions } from '../../model/price-line-options';
 import { LineStyle } from '../../renderers/draw-line';
 
 export const priceLineOptionsDefaults: PriceLineOptions = {
 	color: '#FF0000',
-	price: 0 as BarPrice,
+	price: 0,
 	lineStyle: LineStyle.Dashed,
 	lineWidth: 1,
 	axisLabelVisible: true,
