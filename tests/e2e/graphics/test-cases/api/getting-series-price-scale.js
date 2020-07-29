ac087451635153717b51e922295683dd71b45531
Evgeniy Timokhov <timocov@gmail.com>
2020-07-29 16:54:31 +0300

    Added test to handle getting series price scale

---
+++ tests/e2e/graphics/test-cases/api/getting-series-price-scale.js
@@ -0,0 +1,12 @@
+function runTestCase(container) {
+	const chart = LightweightCharts.createChart(container);
+	const series = chart.addAreaSeries();
+	series.setData([
+		{ time: '1990-04-24', value: 0 },
+		{ time: '1990-04-25', value: 1 },
+		{ time: '1990-04-26', value: 2 },
+		{ time: '1990-04-28', value: 3 },
+	]);
+
+	console.assert(series.priceScale(), 'should be able to return price scale');
+}
