150c5ba18df2148b56f54402637e905fef4b470e
Andree Yosua <andree.yosua@gmail.com>
2020-10-18 11:56:03 +0700

    Add coordinateToLogical and logicalToCoordinate

--- src/api/time-scale-api.ts
+++ src/api/time-scale-api.ts
@@ -5,7 +5,7 @@ import { clone, DeepPartial } from '../helpers/strict-type-checks';
 
 import { ChartModel } from '../model/chart-model';
 import { Coordinate } from '../model/coordinate';
-import { LogicalRange, Range, TimePointsRange } from '../model/time-data';
+import { Logical, LogicalRange, Range, TimePointIndex, TimePointsRange } from '../model/time-data';
 import { TimeScale, TimeScaleOptions } from '../model/time-scale';
 
 import { Time } from './data-consumer';
@@ -96,6 +96,26 @@ export class TimeScaleApi implements ITimeScaleApi, IDestroyable {
 
 	public fitContent(): void {
 		this._model.fitContent();
+	}
+
+	public logicalToCoordinate(logical: Logical): Coordinate | null {
+		const timeScale = this._model.timeScale();
+
+		if (timeScale.visibleLogicalRange() === null) {
+			return null;
+		} else {
+			return timeScale.indexToCoordinate(logical as unknown as TimePointIndex);
+		}
+	}
+
+	public coordinateToLogical(x: number): Logical | null {
+		const timeScale = this._model.timeScale();
+
+		if (timeScale.visibleLogicalRange() === null) {
+			return null;
+		} else {
+			return timeScale.coordinateToIndex(x as Coordinate) as unknown as Logical;
+		}
 	}
 
 	public timeToCoordinate(time: Time): Coordinate | null {
