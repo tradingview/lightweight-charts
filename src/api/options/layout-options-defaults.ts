54c20dfb087cd7805df22c859d100c434d4d9481
Eugene Korobko <ekorobko@tradingview.com>
2020-11-06 13:30:59 +0300

    606: Temporary restored font size

--- src/api/options/layout-options-defaults.ts
+++ src/api/options/layout-options-defaults.ts
@@ -5,6 +5,6 @@ import { LayoutOptions } from '../../model/layout-options';
 export const layoutOptionsDefaults: LayoutOptions = {
 	backgroundColor: '#FFFFFF',
 	textColor: '#191919',
-	fontSize: 12,
+	fontSize: 11,
 	fontFamily: defaultFontFamily,
 };
