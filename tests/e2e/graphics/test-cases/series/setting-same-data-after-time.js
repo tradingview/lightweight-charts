633a9307acef8d9f37ce3ceacfe03db15b17844d
Evgeniy Timokhov <timocov@gmail.com>
2020-11-13 19:14:07 +0300

    Added price range invalidation after removing the last series from price scale
    Fixes #615

---
+++ tests/e2e/graphics/test-cases/series/setting-same-data-after-time.js
@@ -0,0 +1,38 @@
+function copy(data) {
+	return JSON.parse(JSON.stringify(data));
+}
+
+function runTestCase(container) {
+	const chart = LightweightCharts.createChart(container);
+	const lineSeries = chart.addLineSeries();
+
+	const data = [
+		{ time: '2019-04-11', value: 80.01 },
+		{ time: '2019-04-12', value: 96.63 },
+		{ time: '2019-04-13', value: 76.64 },
+		{ time: '2019-04-14', value: 81.89 },
+		{ time: '2019-04-15', value: 74.43 },
+		{ time: '2019-04-16', value: 80.01 },
+		{ time: '2019-04-17', value: 96.63 },
+		{ time: '2019-04-18', value: 76.64 },
+		{ time: '2019-04-19', value: 81.89 },
+		{ time: '2019-04-20', value: 74.43 },
+	];
+
+	lineSeries.setData(copy(data));
+
+	return new Promise(resolve => {
+		setTimeout(() => {
+			chart.removeSeries(lineSeries);
+
+			setTimeout(() => {
+				const newLineSeries = chart.addLineSeries();
+
+				setTimeout(() => {
+					newLineSeries.setData(copy(data));
+					resolve();
+				}, 200);
+			}, 200);
+		}, 200);
+	});
+}
