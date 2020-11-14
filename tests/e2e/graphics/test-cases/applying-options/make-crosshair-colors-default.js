5336c7eaf7be394c7d56ecb85427d0faae87ae12
Manuel Cepeda <mecm1993@gmail.com>
2020-11-13 21:51:53 -0400

    Add tests for crosshair new properties

---
+++ tests/e2e/graphics/test-cases/applying-options/make-crosshair-colors-default.js
@@ -0,0 +1,44 @@
+function generateData() {
+	const res = [];
+	const time = new Date(Date.UTC(2018, 0, 1, 0, 0, 0, 0));
+	for (let i = 0; i < 500; ++i) {
+		res.push({
+			time: time.getTime() / 1000,
+			value: i,
+		});
+
+		time.setUTCDate(time.getUTCDate() + 1);
+	}
+	return res;
+}
+
+function runTestCase(container) {
+	const chart = LightweightCharts.createChart(container);
+	
+	const areaSeries = chart.addAreaSeries({
+        crosshairMarkerBorderColor: 'yellow',
+        crosshairMarkerBackgroundColor: 'red',
+	});
+
+	const lineSeries = chart.addLineSeries({
+        crosshairMarkerBorderColor: 'blue',
+        crosshairMarkerBackgroundColor: 'green',
+	});
+
+	areaSeries.setData(generateData());
+	lineSeries.setData(generateData());
+
+	return new Promise(resolve => {
+		setTimeout(() => {
+			areaSeries.applyOptions({
+                crosshairMarkerBorderColor: '#ffffff',
+                crosshairMarkerBackgroundColor: '#2296f3',
+			});
+			lineSeries.applyOptions({
+                crosshairMarkerBorderColor: '#ffffff',
+                crosshairMarkerBackgroundColor: '#2296f3',
+			});
+			resolve();
+		}, 300);
+	});
+}
