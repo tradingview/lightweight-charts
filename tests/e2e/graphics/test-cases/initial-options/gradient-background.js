0d6b2a6480fd5ba0187a410e09c597a20eb4e90f
Kirill Chetverikov <kchetverikov@tradingview.com>
2021-08-25 17:38:17 +0300

    Gradient background: added graphic tests

---
+++ tests/e2e/graphics/test-cases/initial-options/gradient-background.js
@@ -0,0 +1,30 @@
+function generateData() {
+	const res = [];
+	const time = new Date(Date.UTC(2018, 0, 1, 0, 0, 0, 0));
+	for (let i = 0; i < 500; ++i) {
+		res.push({
+			time: time.getTime() / 1000,
+			value: i,
+		});
+
+		time.setUTCDate(time.getUTCDate() + 1);
+	}
+
+	return res;
+}
+
+function runTestCase(container) {
+	const chart = LightweightCharts.createChart(container, {
+		layout: {
+			backgroundType: LightweightCharts.ColorType.Gradient,
+			backgroundGradientStartColor: '#b1ff73',
+			backgroundGradientEndColor: '#1106b1',
+		},
+	});
+
+	const series = chart.addLineSeries({
+		title: 'ABCD',
+	});
+
+	series.setData(generateData());
+}
