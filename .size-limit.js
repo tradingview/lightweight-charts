6cd605c11ef707884ebfeec79c6120bf76a95675
Kirill Chetverikov <kchetverikov@tradingview.com>
2021-09-02 18:41:24 +0300

    Increased size limits

--- .size-limit.js
+++ .size-limit.js
@@ -4,11 +4,11 @@ module.exports = [
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.esm.production.js',
-		limit: '42.5 KB',
+		limit: '43 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '43 KB',
+		limit: '43.5 KB',
 	},
 ];
