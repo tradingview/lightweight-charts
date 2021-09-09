142447ced86145e4436ad534c432f19ab181fb51
Adrian Ng <ntf123@gmail.com>
2021-09-10 00:19:33 +0800

    Revert "implement subscribePaneResize and unsubscribePaneResize"
    This reverts commit 2194ebc3

--- src/api/chart-api.ts
+++ src/api/chart-api.ts
@@ -1,9 +1,4 @@
-import {
-	ChartWidget,
-	MouseEventParamsImpl,
-	MouseEventParamsImplSupplier, PaneEventParamsImpl,
-	PaneEventParamsImplSupplier,
-} from '../gui/chart-widget';
+import { ChartWidget, MouseEventParamsImpl, MouseEventParamsImplSupplier } from '../gui/chart-widget';
 
 import { ensureDefined } from '../helpers/assertions';
 import { Delegate } from '../helpers/delegate';
@@ -35,7 +30,7 @@ import {
 import { CandlestickSeriesApi } from './candlestick-series-api';
 import { DataUpdatesConsumer, SeriesDataItemTypeMap } from './data-consumer';
 import { DataLayer, DataUpdateResponse, SeriesChanges } from './data-layer';
-import { IChartApi, MouseEventHandler, MouseEventParams, PaneEventHandler, PaneEventParams } from './ichart-api';
+import { IChartApi, MouseEventHandler, MouseEventParams } from './ichart-api';
 import { IPriceScaleApi } from './iprice-scale-api';
 import { ISeriesApi } from './iseries-api';
 import { ITimeScaleApi } from './itime-scale-api';
@@ -161,7 +156,6 @@ export class ChartApi implements IChartApi, DataUpdatesConsumer<SeriesType> {
 
 	private readonly _clickedDelegate: Delegate<MouseEventParams> = new Delegate();
 	private readonly _crosshairMovedDelegate: Delegate<MouseEventParams> = new Delegate();
-	private readonly _paneResizeDelegate: Delegate<PaneEventParams> = new Delegate();
 
 	private readonly _timeScaleApi: TimeScaleApi;
 
@@ -184,15 +178,6 @@ export class ChartApi implements IChartApi, DataUpdatesConsumer<SeriesType> {
 			(paramSupplier: MouseEventParamsImplSupplier) => {
 				if (this._crosshairMovedDelegate.hasListeners()) {
 					this._crosshairMovedDelegate.fire(this._convertMouseParams(paramSupplier()));
-				}
-			},
-			this
-		);
-
-		this._chartWidget.paneResized().subscribe(
-			(paramSupplier: PaneEventParamsImplSupplier) => {
-				if (this._paneResizeDelegate.hasListeners()) {
-					this._paneResizeDelegate.fire(this._convertPaneResizeParams(paramSupplier()));
 				}
 			},
 			this
@@ -354,14 +339,6 @@ export class ChartApi implements IChartApi, DataUpdatesConsumer<SeriesType> {
 		return this._chartWidget.takeScreenshot();
 	}
 
-	public subscribePaneResize(handler: PaneEventHandler): void {
-		this._paneResizeDelegate.subscribe(handler);
-	}
-
-	public unsubscribePaneResize(handler: PaneEventHandler): void {
-		this._paneResizeDelegate.unsubscribe(handler);
-	}
-
 	public removePane(index: number): void {
 		this._chartWidget.model().removePane(index);
 	}
@@ -397,20 +374,6 @@ export class ChartApi implements IChartApi, DataUpdatesConsumer<SeriesType> {
 			hoveredSeries,
 			hoveredMarkerId: param.hoveredObject,
 			seriesPrices,
-		};
-	}
-
-	// noinspection JSMethodCanBeStatic
-	private _convertPaneResizeParams(param: PaneEventParamsImpl): PaneEventParams {
-		return {
-			top: {
-				index: param.top.index,
-				height: param.top.height,
-			},
-			bottom: {
-				index: param.bottom.index,
-				height: param.bottom.height,
-			},
 		};
 	}
 }
