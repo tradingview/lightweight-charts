82f8274ea81a5be5875fd64e4b1152542d5e7d42
Romain Francois <rfrancois@tradingview.com>
2022-01-06 12:04:07 +0000

    Removing unused code

--- website/src/pages/_react/react-components.tsx
+++ website/src/pages/_react/react-components.tsx
@@ -16,60 +16,6 @@ export interface OHLCDataType {
 
 export type ChartDataType = NonOHLCDataType | OHLCDataType;
 
-export const CodeComponent = (options: { typeOfChart: SeriesType; seriesType: string | null }): JSX.Element => {
-	const { seriesType, typeOfChart } = options;
-
-	const code = `
-		// Here's a piece of code that could be placed in a separate file like 'ChartComponent.tsx'
-		export const ChartComponent = (options: { data }) => {
-			const chart = createChart(containerId, {
-				width: 400,
-				height: 300,
-				rightPriceScale: {
-					visible: true
-				},
-			});
-
-			const series = chart.${seriesType}();
-			series.setData(data);
-		}
-
-		// When used as a component, you could 'import' your component into another one
-		import { ChartComponent } from './ChartComponent';
-
-		...
-		export const MyNewComponent = () => {
-			// Careful as for some type data would have to be OHLC formatted as in
-			// { time: '2018-12-22', open: 162.81, high: 167.96, low: 160.17, close: 160.48 }
-			const myData = [
-				{ time: '2018-12-22', value: 32.51 },
-				{ time: '2018-12-23', value: 31.11 },
-				{ time: '2018-12-24', value: 27.02 },
-				{ time: '2018-12-25', value: 27.32 },
-				{ time: '2018-12-26', value: 25.17 },
-				{ time: '2018-12-27', value: 28.89 },
-				{ time: '2018-12-28', value: 25.46 },
-				{ time: '2018-12-29', value: 23.92 },
-				{ time: '2018-12-30', value: 22.68 },
-				{ time: '2018-12-31', value: 22.67 },
-			];
-
-			return (
-				<ChartComponent typeOfChart={'${typeOfChart}'} data={myData} />
-			);
-		};
-	`;
-
-	return (
-		<>
-			<label>
-				This is the piece of code used to render that chart
-				<textarea readOnly cols={110} rows={40} value={code}/>
-			</label>
-		</>
-	);
-};
-
 export interface ChartProperties {
 	typeOfChart: SeriesType;
 	data: ChartDataType[];
