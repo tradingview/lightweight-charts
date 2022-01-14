dd7c825945dc881714dbb20b80b4c2ef7b8de94f
Edward Dewhurst <edewhurst@tradingview.com>
2022-01-14 16:59:33 +0000

    Increase size limits

--- .size-limit.js
+++ .size-limit.js
@@ -4,11 +4,11 @@ module.exports = [
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.esm.production.js',
-		limit: '44.1 KB',
+		limit: '44.19 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '44.7 KB',
+		limit: '44.74 KB',
 	},
 ];
