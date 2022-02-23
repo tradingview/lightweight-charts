8ddda457ac3017a5d3ff6c4e3f5c3716da0bfb0b
Edward Dewhurst <edewhurst@tradingview.com>
2022-02-23 17:00:36 +0000

    Extract walkLineSegment function and update size limit

--- src/renderers/walk-line.ts
+++ src/renderers/walk-line.ts
@@ -21,47 +21,68 @@ export function walkLine(
 	ctx.moveTo(x, y);
 
 	for (let i = visibleRange.from + 1; i < visibleRange.to; ++i) {
-		const currItem = points[i];
+		walkLineSegment(ctx, points, i, lineType);
+	}
+}
 
-		switch (lineType) {
-			case LineType.Simple:
-				ctx.lineTo(currItem.x, currItem.y);
-				break;
-			case LineType.WithSteps: {
-				ctx.lineTo(currItem.x, points[i - 1].y);
-				ctx.lineTo(currItem.x, currItem.y);
-				break;
-			}
-			case LineType.Curved: {
-				const [cp1, cp2] = getControlPoints(points, i - 1);
-				ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, currItem.x, currItem.y);
-				break;
-			}
+export function walkLineSegment(
+	ctx: CanvasRenderingContext2D,
+	points: readonly LinePoint[],
+	pointIndex: number,
+	lineType: LineType
+): void {
+	const currItem = points[pointIndex];
+
+	switch (lineType) {
+		case LineType.Simple:
+			ctx.lineTo(currItem.x, currItem.y);
+			break;
+		case LineType.WithSteps:
+			ctx.lineTo(currItem.x, points[pointIndex - 1].y);
+			ctx.lineTo(currItem.x, currItem.y);
+			break;
+		case LineType.Curved: {
+			const [cp1, cp2] = getControlPoints(points, pointIndex - 1);
+			ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, currItem.x, currItem.y);
+			break;
 		}
 	}
 }
 
 const curveTension = 6;
 
-const subtract = (p1: LinePoint, p2: LinePoint): LinePoint => {
+function subtract(p1: LinePoint, p2: LinePoint): LinePoint {
 	return { x: p1.x - p2.x as Coordinate, y: p1.y - p2.y as Coordinate };
-};
+}
 
-const add = (p1: LinePoint, p2: LinePoint) => {
+function add(p1: LinePoint, p2: LinePoint): LinePoint {
 	return { x: p1.x + p2.x as Coordinate, y: p1.y + p2.y as Coordinate };
-};
+}
 
-const divide = (p1: LinePoint, n: number) => {
+function divide(p1: LinePoint, n: number): LinePoint {
 	return { x: p1.x / n as Coordinate, y: p1.y / n as Coordinate };
-};
+}
 
-export function getControlPoints(points: readonly LinePoint[], pointIndex: number): [LinePoint, LinePoint] {
-	const currentPointIndex = pointIndex;
-	const nextPointIndex = pointIndex + 1;
-	const previousPointIndex = Math.max(0, pointIndex - 1);
-	const nextNextPointIndex = Math.min(points.length - 1, pointIndex + 2);
-	const cp1 = add(points[currentPointIndex], divide(subtract(points[nextPointIndex], points[previousPointIndex]), curveTension));
-	const cp2 = subtract(points[nextPointIndex], divide(subtract(points[nextNextPointIndex], points[currentPointIndex]), curveTension));
+/**
+  * Returns the two control points used to draw a curve between the points at `points[pointIndex]` and `points[pointIndex + 1]`.
+  *
+  * The control points are calculated using 4 points: `points[pointIndex - 1]`, `points[pointIndex]`, `points[pointIndex + 1]` and `points[pointIndex + 2]`.
+  *
+  * If any of these indexes would under/overflow the points array (for example `pointIndex + 2` when `pointIndex = points.length - 1`) then they are clamped
+  * to 0 or `points.length - 1`.
+  *
+  * The calculation of the control points is based on a Catmull-Rom spline expressed as a Bezier curve.
+  *
+  * @param points - The array of points.
+  * @param pointIndex - The index in `points` of the point used as the start of the curve.
+  */
+function getControlPoints(points: readonly LinePoint[], pointIndex: number): [LinePoint, LinePoint] {
+	const startPointIndex = pointIndex;
+	const endPointIndex = pointIndex + 1;
+	const beforeStartPointIndex = Math.max(0, pointIndex - 1);
+	const afterEndPointIndex = Math.min(points.length - 1, pointIndex + 2);
+	const cp1 = add(points[startPointIndex], divide(subtract(points[endPointIndex], points[beforeStartPointIndex]), curveTension));
+	const cp2 = subtract(points[endPointIndex], divide(subtract(points[afterEndPointIndex], points[startPointIndex]), curveTension));
 
 	return [cp1, cp2];
 }
