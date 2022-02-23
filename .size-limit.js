05c1d0e1f25ffcbba2803a7be912f0466af46413
Edward Dewhurst <edewhurst@tradingview.com>
2022-02-23 15:40:08 +0000

    Adjust size limit after refactoring

--- .size-limit.js
+++ .size-limit.js
@@ -4,11 +4,11 @@ module.exports = [
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.esm.production.js',
-		limit: '45.6 KB',
+		limit: '45.4 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '46.2 KB',
+		limit: '45.9 KB',
 	},
 ];
