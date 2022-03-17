fd2825d5fa052954529ce623efa3822b4a3a12c1
Edward Dewhurst <edewhurst@tradingview.com>
2022-03-17 13:33:34 +0000

    Handle iOS Safari iframe postmessage behaviour

--- website/plugins/chart-codeblock/theme/CodeBlock/index.jsx
+++ website/plugins/chart-codeblock/theme/CodeBlock/index.jsx
@@ -29,6 +29,7 @@ function getSrcDocWithScript(script, parentOrigin) {
 			};
 
 			window.parent.postMessage('ready', '${parentOrigin}');
+			window.__READY_TO_RUN = true;
 		</script>
 	`;
 }
@@ -36,13 +37,50 @@ function getSrcDocWithScript(script, parentOrigin) {
 const Chart = props => {
 	const { script } = props;
 	const { origin } = window;
+	const { version } = useDocsVersion();
 	const srcDoc = getSrcDocWithScript(script, origin);
 	const ref = React.useRef();
 
-	const { version } = useDocsVersion();
-
+	/**
+	 * iOS Safari seems to run scripts iwthin the iframe in a differnet order
+	 * compared to desktop Chrome, Safari, etc.
+	 *
+	 * On the desktop browsers the React effect will run first, so the 'ready'
+	 * event listener is added before the iframe script calls postMessage.
+	 *
+	 * On iOS Safari the iframe script runs before the React effect, so the
+	 * 'ready' event listener hasn't been added yet!
+	 *
+	 * We use the __READY_TO_RUN flag to handle this case. If __READY_TO_RUN
+	 * is true then we know that the inner window's run function can be
+	 * called immediately and we don't need to register a event listener.
+	 */
 	React.useEffect(() => {
 		const lightweightChartsImportPromise = importLightweightChartsVersion(version);
+		const injectCreateChartAndRun = contentWindow => {
+			lightweightChartsImportPromise.then(mod => {
+				const createChart = mod.createChart;
+
+				contentWindow.createChart = (container, options) => {
+					const chart = createChart(container, options);
+					const resizeListener = () => {
+						const boundingClientRect = container.getBoundingClientRect();
+						chart.resize(boundingClientRect.width, boundingClientRect.height);
+					};
+
+					contentWindow.addEventListener('resize', resizeListener, true);
+
+					return chart;
+				};
+
+				contentWindow.run();
+			});
+		};
+
+		if (ref.current.contentWindow.__READY_TO_RUN) {
+			injectCreateChartAndRun(ref.current.contentWindow);
+			return;
+		}
 
 		const readyMessageListener = event => {
 			if (event.origin !== origin || event.source !== ref.current.contentWindow) {
@@ -50,26 +88,8 @@ const Chart = props => {
 			}
 
 			if (event.data === 'ready') {
+				injectCreateChartAndRun(event.source);
 				window.removeEventListener('message', readyMessageListener, false);
-
-				lightweightChartsImportPromise.then(mod => {
-					const createChart = mod.createChart;
-					const contentWindow = event.source;
-
-					contentWindow.createChart = (container, options) => {
-						const chart = createChart(container, options);
-						const resizeListener = () => {
-							const boundingClientRect = container.getBoundingClientRect();
-							chart.resize(boundingClientRect.width, boundingClientRect.height);
-						};
-
-						contentWindow.addEventListener('resize', resizeListener, true);
-
-						return chart;
-					};
-
-					event.source.run();
-				});
 			}
 		};
 
