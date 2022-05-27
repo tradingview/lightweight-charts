e1798496138f9b1de5e22d9a77cf1af75c2c3df0
Eugene Korobko <ekorobko@tradingview.com>
2022-05-27 13:27:45 +0300

    606: Fixed size limits

--- .size-limit.js
+++ .size-limit.js
@@ -4,11 +4,11 @@ module.exports = [
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.esm.production.js',
-		limit: '43.77 B',
+		limit: '43.77 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '44.32 KB',
+		limit: '44323 B',
 	},
 ];
