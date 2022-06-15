de923d25f9796199befcccf2bc70c867c1578709
Eugene Korobko <ekorobko@tradingview.com>
2022-06-15 16:48:19 +0300

    606: Updated size limits

--- .size-limit.js
+++ .size-limit.js
@@ -4,11 +4,11 @@ module.exports = [
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.esm.production.js',
-		limit: '43.77 KB',
+		limit: '43.8 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '44.33 KB',
+		limit: '44.36 KB',
 	},
 ];
