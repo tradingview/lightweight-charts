af9b729f1f7430c903c93906c67803f0342b60be
Kirill Chetverikov <kchetverikov@tradingview.com>
2023-04-25 20:59:03 +0300

    Updated size limits

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '44.75 KB',
+		limit: '44.55 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '44.65 KB',
+		limit: '44.5 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '46.35 KB',
+		limit: '46.2 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '46.4 KB',
+		limit: '46.25 KB',
 	},
 ];
