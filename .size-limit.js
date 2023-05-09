9cfef09ced1831730076c0ca2eec332822e6485c
Mark Silverwood <msilverwood@gmail.com>
2023-05-09 16:05:34 +0100

    simplify roundRect implementation
    since we only every use the 4 item array option.

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '43.29 KB',
+		limit: '43.18 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '43.21 KB',
+		limit: '43.09 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '44.92 KB',
+		limit: '44.81 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '44.97 KB',
+		limit: '44.85 KB',
 	},
 ];
