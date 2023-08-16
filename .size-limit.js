65b8fe1f14f8283cd44566f23b83ea93b9ddfac0
Mark Silverwood <msilverwood@gmail.com>
2023-08-16 14:56:20 +0100

    update size-limit after master merge
    Size limit exceeded by 4 B

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '47.58 KB',
+		limit: '47.59 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '47.53 KB',
+		limit: '47.54 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '49.25 KB',
+		limit: '49.26 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '49.3 KB',
+		limit: '49.31 KB',
 	},
 ];
