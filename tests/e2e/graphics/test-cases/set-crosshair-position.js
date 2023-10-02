c57ee7984774c8d422863e7adeae5058edcbfdbf
Ed Dewhurst <edewhurst@tradingview.com>
2023-10-02 13:08:34 +0100

    Add test case

---
+++ tests/e2e/graphics/test-cases/set-crosshair-position.js
@@ -0,0 +1,25 @@
+function generateData() {
+	const res = [];
+	const time = new Date(Date.UTC(2023, 0, 1, 0, 0, 0, 0));
+
+	for (let i = 0; i < 12; ++i) {
+		res.push({
+			time: time.getTime() / 1000,
+			value: i,
+		});
+
+		time.setUTCMonth(time.getUTCMonth() + 1);
+	}
+
+	return res;
+}
+
+function runTestCase(container) {
+	const chart = window.chart = LightweightCharts.createChart(container);
+
+	const series = chart.addLineSeries();
+	series.setData(generateData());
+	chart.timeScale().fitContent();
+
+	chart.setCrosshairPosition(Date.UTC(2023, 2, 1, 0, 0, 0, 0) / 1000, 10, series);
+}
