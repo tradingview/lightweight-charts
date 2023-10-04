f9b7eeb4e5f46289ea6a4685ddd626171c985ca8
Mark Silverwood <msilverwood@gmail.com>
2023-10-04 11:19:52 +0100

    Update .size-limit.js

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '47.77 KB',
+		limit: '47.78 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '47.71 KB',
+		limit: '47.72 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '49.42 KB',
+		limit: '49.44 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '49.47 KB',
+		limit: '49.48 KB',
 	},
 ];
