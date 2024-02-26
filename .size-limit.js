4ccf0f5cd3cc9891d3295b82bedf1789f3459bc9
Mark Silverwood <3482679+SlicedSilver@users.noreply.github.com>
2024-02-26 16:26:13 +0000

    add attribution logo option

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '48.07 KB',
+		limit: '48.97 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '48.02 KB',
+		limit: '48.91 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '49.71 KB',
+		limit: '50.62 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '49.76 KB',
+		limit: '50.67 KB',
 	},
 ];
