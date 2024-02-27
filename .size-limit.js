348593ac370fb3d2cea3952bf7b83cd7977ec093
Mark Silverwood <3482679+SlicedSilver@users.noreply.github.com>
2024-02-27 12:36:29 +0000

    drop support for commonjs

--- .size-limit.js
+++ .size-limit.js
@@ -2,23 +2,18 @@
 
 module.exports = [
 	{
-		name: 'CJS',
-		path: 'dist/lightweight-charts.production.cjs',
-		limit: '47.98 KB',
-	},
-	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '47.91 KB',
+		limit: '48.02 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '49.63 KB',
+		limit: '49.72 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '49.67 KB',
+		limit: '49.76 KB',
 	},
 ];
