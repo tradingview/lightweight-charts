c384ad32bf81e84be42beda2c763fd3abcb9bf62
Mark Silverwood <3482679+SlicedSilver@users.noreply.github.com>
2024-02-27 13:19:05 +0000

    update typescript target to es2020

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '47.98 KB',
+		limit: '47.25 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '47.91 KB',
+		limit: '47.14 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '49.63 KB',
+		limit: '48.93 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '49.67 KB',
+		limit: '48.99 KB',
 	},
 ];
