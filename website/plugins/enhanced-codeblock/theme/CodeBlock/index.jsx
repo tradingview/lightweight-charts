0e70d460361100a221e7441f75f3fb007c5e1bfa
Andrii Ovcharenko <illetid@gmail.com>
2024-03-07 13:37:22 +0100

    feature: add replaceTabs props to CodeBlock

--- website/plugins/enhanced-codeblock/theme/CodeBlock/index.jsx
+++ website/plugins/enhanced-codeblock/theme/CodeBlock/index.jsx
@@ -24,12 +24,16 @@ export function replaceThemeConstantStrings(originalString, isDarkTheme) {
 	return result;
 }
 
+export function replaceTabsString(string) {
+	return string.replace(/\t/g, '    ');
+}
+
 export function removeUnwantedLines(originalString) {
 	return originalString.replace(new RegExp(/\/\/ delete-start[\w\W]*?\/\/ delete-end/, 'gm'), '');
 }
 
 const EnhancedCodeBlock = props => {
-	const { chart, replaceThemeConstants, hideableCode, chartOnly, iframeStyle, ...rest } = props;
+	const { chart, replaceThemeConstants, hideableCode, chartOnly, iframeStyle, replaceTabs, ...rest } = props;
 	let { children } = props;
 	const { colorMode } = useColorMode();
 	const isDarkTheme = colorMode === 'dark';
@@ -37,6 +41,9 @@ const EnhancedCodeBlock = props => {
 
 	if (replaceThemeConstants && typeof children === 'string') {
 		children = replaceThemeConstantStrings(children, isDarkTheme);
+	}
+	if (replaceTabs && typeof children === 'string') {
+		children = replaceTabsString(children);
 	}
 	children = removeUnwantedLines(children);
 
