bead8cfed830c0dc500b67e75932b3f4ee69f7c5
Mark Silverwood <3482679+SlicedSilver@users.noreply.github.com>
2024-05-08 16:50:27 +0100

    update size-limit

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '48.16 KB',
+		limit: '48.17 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '48.09 KB',
+		limit: '48.10 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '49.79 KB',
+		limit: '49.80 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '49.83 KB',
+		limit: '49.84 KB',
 	},
 ];
