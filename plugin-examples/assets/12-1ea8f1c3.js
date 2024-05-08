1f0f3a48824d4072b7a4f031ac078b869073006e
TradingView <noreply@tradingview.com>
2024-05-08 16:17:28 +0000

    [skip ci] Deploy website on 2fd496e1f42352e6189221cc5cf81b6d84da54b9 2fd496e1f42352e6189221cc5cf81b6d84da54b9

--- plugin-examples/assets/12-1ea8f1c3.js
+++
@@ -1,1 +0,0 @@
-import{T as i}from"./lightweight-charts.production-a04f98bb.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-f4a48077.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
---
+++ plugin-examples/assets/12-9547066d.js
@@ -0,0 +1,1 @@
+import{T as i}from"./lightweight-charts.production-9fea55e1.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-75c4e04c.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
