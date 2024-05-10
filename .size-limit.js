fb3bb050500efdfa96533d245177d854a0c652ff
Andrii Ovcharenko <illetid@gmail.com>
2024-05-10 16:14:07 +0200

    update .sizelimit

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '48.24 KB',
+		limit: '48.27 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '48.18 KB',
+		limit: '48.2 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '49.87 KB',
+		limit: '49.9 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '49.92 KB',
+		limit: '49.95 KB',
 	},
 ];
