bccb4290abddef4cf8467970a945e2f523901343
Mark Silverwood <3482679+SlicedSilver@users.noreply.github.com>
2024-06-06 18:45:18 +0100

    add how to example for horizontal price scale

--- .eslintrc.js
+++ .eslintrc.js
@@ -235,6 +235,7 @@ module.exports = {
 						'@typescript-eslint/no-misused-promises': 'off',
 						'@typescript-eslint/no-unnecessary-qualifier': 'off',
 						'@typescript-eslint/no-unnecessary-type-assertion': 'off',
+						'@typescript-eslint/no-unsafe-argument': 'off',
 						'@typescript-eslint/no-unsafe-call': 'off',
 						'@typescript-eslint/no-unsafe-member-access': 'off',
 						'@typescript-eslint/no-unsafe-return': 'off',
--- website/tutorials/how_to/.eslintrc.js
+++ website/tutorials/how_to/.eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
 	globals: {
 		document: false,
 		createChart: false,
+		createChartEx: false,
 	},
 };
