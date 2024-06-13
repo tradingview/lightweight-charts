b9d9ff58cce2076e48d100f0064c40b59af1f79d
Mark Silverwood <3482679+SlicedSilver@users.noreply.github.com>
2024-06-13 22:45:43 +0100

    fix generated dts interface names

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,6 @@ export const customSeriesDefaultOptions: CustomSeriesOptions = {
 
 export { createChart, createChartEx } from './api/create-chart';
 
-export { Range } from './model/time-data';
-
 /**
  * Returns the current version as a string. For example `'3.3.0'`.
  */
