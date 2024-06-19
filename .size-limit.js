7108f6b70ff2407e6c1dbac3f6b5e6d619fa7d5b
Eugene Korobko <ekorobko@tradingview.com>
2024-06-19 19:04:21 +0300

    1620: Ceil size limits

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '48262 B',
+		limit: '48.27 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '48174 B',
+		limit: '48.18 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '49.91 KB',
+		limit: '49.92 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '49.95 KB',
+		limit: '49.96 KB',
 	},
 ];
