b84e06381e045f13dab6e151ea72d884fe36eae9
Mark Silverwood <3482679+SlicedSilver@users.noreply.github.com>
2024-07-25 21:42:17 +0100

    Update .size-limit.js

--- .size-limit.js
+++ .size-limit.js
@@ -4,21 +4,21 @@ module.exports = [
 	{
 		name: 'CJS',
 		path: 'dist/lightweight-charts.production.cjs',
-		limit: '49.27 KB',
+		limit: '49.28 KB',
 	},
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.production.mjs',
-		limit: '49.20 KB',
+		limit: '49.21 KB',
 	},
 	{
 		name: 'Standalone-ESM',
 		path: 'dist/lightweight-charts.standalone.production.mjs',
-		limit: '50.92 KB',
+		limit: '50.93 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '50.96 KB',
+		limit: '50.97 KB',
 	},
 ];
