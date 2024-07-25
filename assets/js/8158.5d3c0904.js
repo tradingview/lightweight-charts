b890e651884358b6357f7a5feb6addb40d73d1eb
TradingView <noreply@tradingview.com>
2024-07-25 17:51:40 +0000

    [skip ci] Deploy website on 6e70f5487f4ab52d1d2ebc263c9e7c4a8969c37c 6e70f5487f4ab52d1d2ebc263c9e7c4a8969c37c

---
+++ assets/js/8158.5d3c0904.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[8158],{48158:(a,e,c)=>{c.r(e),c.d(e,{DocSearchModal:()=>s.a1});var s=c(55600)}}]);
--- plugin-examples/assets/12-30181f54.js
+++
@@ -1,1 +0,0 @@
-import{P as i}from"./lightweight-charts.production-72079ca6.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-e0c5066d.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
