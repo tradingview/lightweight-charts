1a7dac2475ddf0a01e6b066597d4c8cedca6d0b1
Aayush Ahuja <aayushahuja.in@gmail.com>
2024-09-13 18:56:21 -0700

    more changes

--- scripts/stock_and_options_eod.py
+++ scripts/stock_and_options_eod.py
@@ -78,8 +78,9 @@ Stock EOD api:
 import http.client
 import json
 from datetime import datetime, timedelta
+from typing import List
 
-def get_expirations(ticker: str) -> list[str]:
+def get_expirations(ticker: str) -> List[str]:
     conn = http.client.HTTPConnection("127.0.0.1:25510")
     headers = { 'Accept': "application/json" }
     conn.request("GET", f"/v2/list/expirations?root={ticker}", headers=headers)
@@ -93,6 +94,54 @@ def get_expirations(ticker: str) -> list[str]:
     conn.close()
     return expirations
 
+
+def get_strikes(ticker: str, expiration: str, current_price: float, strike_range: float = 0.05) -> List[str]:
+    conn = http.client.HTTPConnection("127.0.0.1:25510")
+    headers = { 'Accept': "application/json" }
+    conn.request("GET", f"/v2/list/strikes?root={ticker}&exp={expiration}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    data_json = json.loads(data.decode("utf-8"))
+    # import pdb; pdb.set_trace()
+    strikes = [float(strike) / 1000 for strike in data_json["response"]]
+    # only use the strikes that are within 20% of the current day close price
+    strikes = [strike for strike in strikes if strike >= current_price * (1 - strike_range) and strike <= current_price * (1 + strike_range)]
+    conn.close()
+    return strikes
+
+def get_option_prices(ticker: str, strike: float, expiration: str) -> List[float]:
+    conn = http.client.HTTPConnection("127.0.0.1:25510")
+    headers = { 'Accept': "application/json" }
+    # start and end date are current date
+    start_date = datetime.now().strftime("%Y%m%d")
+    end_date = datetime.now().strftime("%Y%m%d")
+    conn.request("GET", f"/v2/hist/option/eod?exp={expiration}&right=C&strike={strike}&start_date={start_date}&end_date={end_date}&root={ticker}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    data_json = json.loads(data.decode("utf-8"))
+    return data_json["response"]
+
+def get_stock_prices(ticker: str) -> List[float]:
+    conn = http.client.HTTPConnection("127.0.0.1:25510")
+    headers = { 'Accept': "application/json" }
+    # start and end date are current date
+    start_date = datetime.now().strftime("%Y%m%d")
+    end_date = datetime.now().strftime("%Y%m%d")
+    conn.request("GET", f"/v2/hist/stock/eod?root={ticker}&start_date={start_date}&end_date={end_date}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    data_json = json.loads(data.decode("utf-8"))
+    return data_json["response"]
+
 if __name__ == "__main__":
-    expirations = get_expirations("AAPL")
+    ticker = "AAPL"
+    expirations = get_expirations(ticker)
     print(expirations)
+    current_price = 222.5
+    strikes = get_strikes(ticker, expirations[0], current_price)
+    print(strikes)
+    # for each strike, get the option prices for all the expirations
+    for strike in strikes:
+        for expiration in expirations:
+            option_prices = get_option_prices(ticker, strike, expiration)
+            print(option_prices)
