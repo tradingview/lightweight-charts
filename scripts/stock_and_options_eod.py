59557961589c40f7cc027d9a01bc87e170f46ac3
Aayush Ahuja <aayushahuja.in@gmail.com>
2024-09-13 19:06:08 -0700

    more changes

--- scripts/stock_and_options_eod.py
+++ scripts/stock_and_options_eod.py
@@ -79,6 +79,7 @@ import http.client
 import json
 from datetime import datetime, timedelta
 from typing import List
+import csv
 
 def get_expirations(ticker: str) -> List[str]:
     conn = http.client.HTTPConnection("127.0.0.1:25510")
@@ -118,20 +119,34 @@ def get_option_prices(ticker: str, strike: float, expiration: str) -> List[float
     conn.request("GET", f"/v2/hist/option/eod?exp={expiration}&right=C&strike={strike}&start_date={start_date}&end_date={end_date}&root={ticker}", headers=headers)
     res = conn.getresponse()
     data = res.read()
-    data_json = json.loads(data.decode("utf-8"))
-    return data_json["response"]
+    data_json = json.loads(data.decode("utf-8"))['response']
+    import pdb; pdb.set_trace()
+    bid = [x[10] for x in data_json]
+    ask = [x[14] for x in data_json]
+    mid = [(bid[i] + ask[i]) / 2 for i in range(len(bid))]
+    assert len(mid) == 1, "More than one price found for the option"
+    return mid[0]
 
 def get_stock_prices(ticker: str) -> List[float]:
     conn = http.client.HTTPConnection("127.0.0.1:25510")
     headers = { 'Accept': "application/json" }
-    # start and end date are current date
-    start_date = datetime.now().strftime("%Y%m%d")
+    # start date is 20 days ago and end date is current date
+    start_date = (datetime.now() - timedelta(days=20)).strftime("%Y%m%d")
     end_date = datetime.now().strftime("%Y%m%d")
     conn.request("GET", f"/v2/hist/stock/eod?root={ticker}&start_date={start_date}&end_date={end_date}", headers=headers)
     res = conn.getresponse()
     data = res.read()
-    data_json = json.loads(data.decode("utf-8"))
-    return data_json["response"]
+    data_json = json.loads(data.decode("utf-8"))['response']
+    return_data = []
+    for x in data_json:
+        import pdb; pdb.set_trace()
+        open = x[2]
+        high = x[3]
+        low = x[4]
+        close = x[5]
+        cur_date = x[-1]
+        return_data.append([cur_date, open, high, low, close])
+    return return_data
 
 if __name__ == "__main__":
     ticker = "AAPL"
@@ -141,7 +156,20 @@ if __name__ == "__main__":
     strikes = get_strikes(ticker, expirations[0], current_price)
     print(strikes)
     # for each strike, get the option prices for all the expirations
+    data = []
     for strike in strikes:
         for expiration in expirations:
-            option_prices = get_option_prices(ticker, strike, expiration)
-            print(option_prices)
+            option_closing_price = get_option_prices(ticker, strike, expiration)
+            data.append([ticker, expiration, strike, option_closing_price])
+    print(data)
+    # export the data to a csv file
+    base_path = f"/Users/aayushahuja/Documents/projects/optionx/dump"
+    with open(f"{base_path}/{ticker}_options_eod_{datetime.now().strftime('%Y%m%d')}.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["ticker", "expiration", "strike", "option_closing_price"])
+        writer.writerows(data)
+    stock_data = get_stock_prices(ticker)
+    with open(f"{base_path}/{ticker}_stock_eod_{datetime.now().strftime('%Y%m%d')}.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["date", "open", "high", "low", "close"])
+        writer.writerows(stock_data)
