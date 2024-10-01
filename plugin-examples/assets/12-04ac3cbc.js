46ca26f8bdff93508360ef1f2a94a5d12ce4b4af
TradingView <noreply@tradingview.com>
2024-10-01 14:07:03 +0000

    [skip ci] Deploy website on b600018ab4d8a26f8d2913bfafbb597ffa402a88 b600018ab4d8a26f8d2913bfafbb597ffa402a88

--- plugin-examples/assets/12-04ac3cbc.js
+++
@@ -1,1 +0,0 @@
-import{V as i}from"./lightweight-charts.production-b3f7122b.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-2eaf0809.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
