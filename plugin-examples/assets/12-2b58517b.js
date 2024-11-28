c1f5f011832530c52e48de3b6619628f13e0eeae
TradingView <noreply@tradingview.com>
2024-11-28 17:18:46 +0000

    [skip ci] Deploy website on 24a5761c15bdbcafe15982eca53a1cac984df847 24a5761c15bdbcafe15982eca53a1cac984df847

---
+++ plugin-examples/assets/12-2b58517b.js
@@ -0,0 +1,1 @@
+import{V as i}from"./lightweight-charts.production-74e5a5e5.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-1a473206.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
