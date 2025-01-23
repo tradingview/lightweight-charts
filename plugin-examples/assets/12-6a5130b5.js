8e3171de970186ef1fb60062fa69e588d8e33119
TradingView <noreply@tradingview.com>
2025-01-23 19:00:35 +0000

    [skip ci] Deploy website on 51ba24010d535bd43f4c9836e2c296def086a528 51ba24010d535bd43f4c9836e2c296def086a528

---
+++ plugin-examples/assets/12-6a5130b5.js
@@ -0,0 +1,1 @@
+import{V as i}from"./lightweight-charts.production-b767f87e.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-42f08c4b.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
