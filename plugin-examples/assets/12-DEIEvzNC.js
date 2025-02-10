68fe9d36b4a3d0ecba6a0d84c8524336f3cba6b6
TradingView <noreply@tradingview.com>
2025-02-10 15:54:08 +0000

    [skip ci] Deploy website on bcdb3e637dd0154a301b2df043c7b8487eb45047 bcdb3e637dd0154a301b2df043c7b8487eb45047

--- plugin-examples/assets/12-DEIEvzNC.js
+++
@@ -1,1 +0,0 @@
-import{L as i,b as a}from"./lightweight-charts.production-p9I_yePe.js";import{a as r}from"./sample-data-x_8-FZm1.js";import{C as e}from"./highlight-bar-crosshair-BoMrK2nW.js";import"./positions-t2UKkghX.js";const o=window.chart=i("chart",{autoSize:!0}),t=o.addSeries(a);t.setData(r());const s=new e({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(s);
