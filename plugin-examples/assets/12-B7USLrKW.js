a4ded982158b722a4f7879098523eef94d18045b
TradingView <noreply@tradingview.com>
2025-03-24 15:29:40 +0000

    [skip ci] Deploy website on ac7ee9d7bc48b7aabbf34da20e394f63d24766ca ac7ee9d7bc48b7aabbf34da20e394f63d24766ca

---
+++ plugin-examples/assets/12-B7USLrKW.js
@@ -0,0 +1,1 @@
+import{N as i,S as a}from"./lightweight-charts.production-COR0OEE9.js";import{a as r}from"./sample-data-x_8-FZm1.js";import{C as e}from"./highlight-bar-crosshair-CqvNjmDh.js";import"./positions-t2UKkghX.js";const o=window.chart=i("chart",{autoSize:!0}),t=o.addSeries(a);t.setData(r());const s=new e({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(s);
