c16a90ee59117c253fd26ad38a545f58bbaa2f91
TradingView <noreply@tradingview.com>
2025-05-16 18:10:35 +0000

    [skip ci] Deploy website on 63ce2ad6d0d15a750dcc5c0379250008f7a5b2bb 63ce2ad6d0d15a750dcc5c0379250008f7a5b2bb

---
+++ plugin-examples/assets/12-DXpv_iAz.js
@@ -0,0 +1,1 @@
+import{N as i,x as a}from"./lightweight-charts.production-DxDuMwk8.js";import{a as r}from"./sample-data-x_8-FZm1.js";import{C as e}from"./highlight-bar-crosshair-BvIAwG4Q.js";import"./positions-t2UKkghX.js";const o=window.chart=i("chart",{autoSize:!0}),t=o.addSeries(a);t.setData(r());const s=new e({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(s);
