e636599a91dfa66d6c278015e0d30e602314f1a4
TradingView <noreply@tradingview.com>
2025-06-26 11:52:08 +0000

    [skip ci] Deploy website on c65d0697ad0c546301a1734912ca50d031d468a2 c65d0697ad0c546301a1734912ca50d031d468a2

---
+++ plugin-examples/assets/12-B5AVFGCO.js
@@ -0,0 +1,1 @@
+import{F as i,C as a}from"./lightweight-charts.production-B1WzI78V.js";import{a as r}from"./sample-data-x_8-FZm1.js";import{C as e}from"./highlight-bar-crosshair-av0sH3-B.js";import"./positions-t2UKkghX.js";const o=window.chart=i("chart",{autoSize:!0}),t=o.addSeries(a);t.setData(r());const s=new e({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(s);
