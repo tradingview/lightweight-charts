84fef7a8e90bd772811422586d7cbc6d7d3eb252
tpunt <tpunt@hotmail.co.uk>
2025-08-07 17:06:02 +0100

    Fix test data

--- tests/e2e/graphics/test-cases/series-markers/series-markers-inverted.js
+++ tests/e2e/graphics/test-cases/series-markers/series-markers-inverted.js
@@ -1,30 +1,22 @@
-function generateData() {
-	const res = [];
-	const time = new Date(Date.UTC(2018, 0, 1, 0, 0, 0, 0));
-	for (let i = 0; i < 30; ++i) {
-		res.push({
-			time: time.getTime() / 1000,
-			value: i,
-		});
-
-		time.setUTCDate(time.getUTCDate() + 1);
-	}
-	return res;
-}
-
 function runTestCase(container) {
 	const chart = window.chart = LightweightCharts.createChart(container, { layout: { attributionLogo: false } });
 
 	const mainSeries = chart.addSeries(LightweightCharts.HistogramSeries);
 
-	const data = generateData();
-	mainSeries.setData(data);
+	const candlesticks = [
+		{ time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
+		{ time: '2018-12-23', open: 45.12, high: 53.9, low: 45.12, close: 48.09 },
+		{ time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
+		{ time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.5 },
+		{ time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 101.04 },
+	];
+	mainSeries.setData(candlesticks);
 
 	const markers = [
-		{ time: data[0].time, position: 'belowBar', color: 'red', shape: 'arrowUp' },
-		{ time: data[0].time, position: 'aboveBar', color: 'red', shape: 'arrowDown' },
-		{ time: data[1].time, position: 'belowBar', color: 'red', shape: 'arrowUp' },
-		{ time: data[2].time, position: 'aboveBar', color: 'red', shape: 'arrowDown' },
+		{ time: candlesticks[4].time, position: 'belowBar', color: 'red', shape: 'arrowUp' },
+		{ time: candlesticks[4].time, position: 'aboveBar', color: 'red', shape: 'arrowDown' },
+		{ time: candlesticks[3].time, position: 'belowBar', color: 'red', shape: 'arrowUp' },
+		{ time: candlesticks[2].time, position: 'aboveBar', color: 'red', shape: 'arrowDown' },
 	];
 
 	const markerSeries = LightweightCharts.createSeriesMarkers(
