824d375a87b4984fb2c415fc1a56526805e3e824
TradingView <noreply@tradingview.com>
2025-08-29 09:52:53 +0000

    [skip ci] Deploy website on 8e04d656d79dc638335bf0d5113e29093fe5a713 8e04d656d79dc638335bf0d5113e29093fe5a713

--- plugin-examples/assets/13-CQ294m71.js
+++
@@ -1,1 +0,0 @@
-import{F as i,C as a}from"./lightweight-charts.production-wU6WDwYG.js";import{a as r}from"./sample-data-x_8-FZm1.js";import{C as e}from"./highlight-bar-crosshair-lJZmo0yy.js";import"./positions-t2UKkghX.js";const o=window.chart=i("chart",{autoSize:!0}),t=o.addSeries(a);t.setData(r());const s=new e({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(s);
