e23bb2da0a093b9bf3408a2b9b5bc857ee98e62d
TradingView <noreply@tradingview.com>
2025-09-02 12:08:30 +0000

    [skip ci] Deploy website on 19b15b296ed9bc8e2098d4ea46cb046e24116e68 19b15b296ed9bc8e2098d4ea46cb046e24116e68

--- plugin-examples/assets/13-Co4HZsX5.js
+++
@@ -1,1 +0,0 @@
-import{F as i,C as a}from"./lightweight-charts.production-BKO5t_Df.js";import{a as r}from"./sample-data-x_8-FZm1.js";import{C as e}from"./highlight-bar-crosshair-DKhIeefZ.js";import"./positions-t2UKkghX.js";const o=window.chart=i("chart",{autoSize:!0}),t=o.addSeries(a);t.setData(r());const s=new e({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(s);
---
+++ plugin-examples/assets/13-fb3tSlXk.js
@@ -0,0 +1,1 @@
+import{F as i,C as a}from"./lightweight-charts.production-DoOc--1G.js";import{a as r}from"./sample-data-x_8-FZm1.js";import{C as e}from"./highlight-bar-crosshair-DRzNDYTT.js";import"./positions-t2UKkghX.js";const o=window.chart=i("chart",{autoSize:!0}),t=o.addSeries(a);t.setData(r());const s=new e({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(s);
