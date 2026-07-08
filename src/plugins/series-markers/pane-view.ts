5666a7c716a8d00975f211a421eaa3882e063449
Khusan Yusupov <kyusupov@tradingview.com>
2026-07-08 20:05:45 +0500

    fix: resolve first data point with NearestRight in markers pane view

--- src/plugins/series-markers/pane-view.ts
+++ src/plugins/series-markers/pane-view.ts
@@ -229,7 +229,7 @@ export class SeriesMarkersPaneView<HorzScaleItem> implements IPrimitivePaneView 
 			return;
 		}
 		const visibleBarsRange = new RangeImpl(Math.floor(visibleBars.from) as TimePointIndex, Math.ceil(visibleBars.to) as TimePointIndex);
-		const firstValue = this._series.dataByIndex(0);
+		const firstValue = this._series.dataByIndex(0, MismatchDirection.NearestRight);
 		if (firstValue === null) {
 			return;
 		}
