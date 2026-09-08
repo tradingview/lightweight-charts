d79f889f9274f0d74441cf186a84c5eb8e65883f
Mark Silverwood <msilverwood@gmail.com>
2026-09-08 15:34:56 +0100

    Graduate vertical-line to @tradingview/lwc-plugin-vertical-line (CLL-403)

--- packages/lwc-plugin-vertical-line/src/example/example.ts
+++ packages/lwc-plugin-vertical-line/src/example/example.ts
@@ -1,6 +1,6 @@
 import { LineSeries, createChart } from 'lightweight-charts';
-import { generateLineData } from '../../../sample-data';
-import { VertLine } from '@tradingview/lwc-plugin-vertical-line';
+import { generateLineData } from './sample-data';
+import { VertLine } from '../vertical-line';
 
 const chart = ((window as unknown as any).chart = createChart('chart', {
 	autoSize: true,
