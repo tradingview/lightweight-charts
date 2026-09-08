a028e2fef7e3f5111f6507c3c70cf72c7c4b749e
Mark Silverwood <msilverwood@gmail.com>
2026-09-08 16:30:49 +0100

    hlc-area-series: highAreaColor/lowAreaColor, LineWidth, generic horizontal scale

--- packages/lwc-plugin-hlc-area-series/src/example/example.ts
+++ packages/lwc-plugin-hlc-area-series/src/example/example.ts
@@ -1,4 +1,4 @@
-import { WhitespaceData, createChart } from 'lightweight-charts';
+import { CustomSeriesWhitespaceData, Time, createChart } from 'lightweight-charts';
 import { HLCAreaSeries } from '../hlc-area-series';
 import { HLCAreaData } from '../data';
 import { generateAlternativeCandleData } from './sample-data';
@@ -12,5 +12,5 @@ const myCustomSeries = chart.addCustomSeries(customSeriesView, {
 	/* Options */
 });
 
-const data: (HLCAreaData | WhitespaceData)[] = generateAlternativeCandleData(100);
+const data: (HLCAreaData | CustomSeriesWhitespaceData<Time>)[] = generateAlternativeCandleData(100);
 myCustomSeries.setData(data);
--- plugin-examples/src/plugins/hlc-area-series/example/example.ts
+++ plugin-examples/src/plugins/hlc-area-series/example/example.ts
@@ -1,4 +1,4 @@
-import { WhitespaceData, createChart } from 'lightweight-charts';
+import { CustomSeriesWhitespaceData, Time, createChart } from 'lightweight-charts';
 import {
 	HLCAreaSeries,
 	HLCAreaData,
@@ -14,5 +14,5 @@ const myCustomSeries = chart.addCustomSeries(customSeriesView, {
 	/* Options */
 });
 
-const data: (HLCAreaData | WhitespaceData)[] = generateAlternativeCandleData(100);
+const data: (HLCAreaData | CustomSeriesWhitespaceData<Time>)[] = generateAlternativeCandleData(100);
 myCustomSeries.setData(data);
